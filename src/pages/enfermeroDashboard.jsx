776c1d032f51f8ef3b05426e0bee9c8590d9fd9b
Gonzalo2504 <gonzagomezbull@gmail.com>
2024-11-12 01:26:41 -0300

    update index.html, enfermeroDashboard.jsx and medicoDashboard.jsx

--- src/pages/enfermeroDashboard.jsx
+++ src/pages/enfermeroDashboard.jsx
@@ -1,18 +1,50 @@
-import PersistentDrawerLeft from "../components/adminNav";
-import { Box } from "@mui/material";
+import React, { useState } from 'react';
+import PersistentDrawerLeft from '../components/adminNav';
+import PacienteTable from '../components/tables/pacientesTable';
+import { useNavigate } from 'react-router-dom';
+import MedicoTable from '../components/tables/medicosTable';
+import EnfermeroTable from '../components/tables/enfermerosTable';
 
-export default function EnfermeroDashboard() {
+function App() {
+  const [selectedComponent, setSelectedComponent] = useState(' ');
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleMenuSelection = (selection) => {
+    setSelectedComponent(selection);
+  };
+
+  const toggleSidebar = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const renderSelectedComponent = () => {
+    switch (selectedComponent) {
+      case 'Pacientes':
+        return <PacienteTable drawerOpen={drawerOpen} />;
+      case 'Medicos':
+        return <MedicoTable drawerOpen={drawerOpen} />;
+      case 'Enfermeros':
+        return <EnfermeroTable drawerOpen={drawerOpen} />;
+      case 'Cerrar Sesion':
+        localStorage.removeItem('token');
+        navigate('/');
+      default:
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            Puede seleccionar opciones de la barra lateral
+          </div>
+        );
+    }
+  };
+
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(assets/61804.jpg)`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-      }}
-    >
-      <PersistentDrawerLeft />
-    </Box>
+    <div>
+      <PersistentDrawerLeft onMenuSelect={handleMenuSelection} open={drawerOpen} toggleSidebar={toggleSidebar} />
+      {renderSelectedComponent()}
+    </div>
   );
 }
+
+export default App;
+
